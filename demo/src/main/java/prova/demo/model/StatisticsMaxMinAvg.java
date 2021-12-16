86be006bec56aa97eb3e153fdfeb092c9beb011b
AlessioCruciani <91695358+AlessioCruciani@users.noreply.github.com>
2021-12-16 16:34:59 +0100

    Inserimento dei commenti per il Javadoc

--- demo/src/main/java/prova/demo/model/StatisticsMaxMinAvg.java
+++ demo/src/main/java/prova/demo/model/StatisticsMaxMinAvg.java
@@ -1,22 +1,37 @@
 package prova.demo.model;
 
-import java.util.ArrayList;
-import prova.demo.exception.*;
-
-import prova.demo.exception.NoFileExc;
-import prova.demo.service.DropboxFile;
 import prova.demo.service.GetDataFromDropbox;
+import prova.demo.service.DropboxFile;
+import java.util.ArrayList;
 
-public class StatisticsMaxMinAvg {    
+/**
+ * Classe che si occupa delle statistiche di Massimo, Minimo e Media
+ * 
+ * @author Alessio Cruciani
+ * @author Alessandra D'Anna
+ * @version 1.0
+ */
+public class StatisticsMaxMinAvg {
+    /**
+     * Creazione di un oggetto di GetDataFromDropbox
+     * @see GetDataFromDropbox
+     */
     GetDataFromDropbox caricaDati = new GetDataFromDropbox();
+    /**
+     * Arraylist che contiene oggetti di tipo DropboxFile
+     */
     ArrayList<DropboxFile> listaFile = caricaDati.getListaFile();
 
-    public int risultatoMinimo(String estensione) throws NoFileExc{
+    /**
+     * Metodo che cerca la dimensione minima di un tipo di file specificato
+     * 
+     * @param estensione Indica l'estensione del file contenuto nel dropbox
+     * @return Indica la dimensione Minima dei file, cercati per tipo di file
+     */
+    public int risultatoMinimo(String estensione) {
         int minimo = 0;
-      if(listaFile.isEmpty()) throw new NoFileExc();
-      else {
         for (DropboxFile i : listaFile) {
-            if ((estensione).equals(i.getEstensione())) {
+            if (estensione.equals(i.getEstensione())) {
                 while (minimo == 0) {
                     minimo = (int) i.getSize();
                 }
@@ -26,37 +41,39 @@ public class StatisticsMaxMinAvg {
             }
         }
         return minimo;
-      }   
     }
-
-    public int risultatoMassimo(String estensione) throws NoFileExc{
+    /**
+     * Metodo che cerca la dimensione massima di un tipo di file specificato
+     * 
+     * @param estensione Indica l'estensione del file contenuto nel dropbox
+     * @return Indica la dimensione Massima dei file, cercati per tipo di file
+     */
+    public int risultatoMassimo(String estensione) {
         int massimo = 0;
-        if(listaFile.isEmpty()) throw new NoFileExc();
-        else {
-        	 for (DropboxFile i : listaFile) {
-                 if((estensione).equals(i.getEstensione())){
-                 if ((int) i.getSize() > massimo) {
-                     massimo = (int) i.getSize();
-                 }
-                 }
-             }
-             return massimo;
+        for (DropboxFile i : listaFile) {
+            if(estensione.equals(i.getEstensione())){
+            if ((int) i.getSize() > massimo) {
+                massimo = (int) i.getSize();
+            }
+            }
         }
+        return massimo;
     }
-
-    public double risultatoMedia(String estensione) throws NoFileExc{
+    /**
+     * Metodo che cerca la dimensione media di un tipo di file specificato
+     * 
+     * @param estensione Indica l'estensione del file contenuto nel dropbox
+     * @return Indica la dimensione Media dei file, cercati per tipo di file
+     */
+    public double risultatoMedia(String estensione) {
         int somma = 0;
         int contatore = 0;
-        if(listaFile.isEmpty()) throw new NoFileExc();
-        else {
-        	
-        	 for(DropboxFile i : listaFile){
-                 if((estensione).equals(i.getEstensione())){
-                     somma += (int) i.getSize();
-                     contatore++;
-                 }
-             }
-             return (somma/contatore);
+        for(DropboxFile i : listaFile){
+            if(estensione.equals(i.getEstensione())){
+                somma += (int) i.getSize();
+                contatore++;
+            }
         }
+        return (somma/contatore);
     }
 }
