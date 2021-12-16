0403850548e0eddc8a4520a51c51f48676f3bef1
Alessandra D'Anna <93262359+aledanna00@users.noreply.github.com>
2021-12-16 16:53:21 +0100

    Update NoExtExc.java

--- demo/src/main/java/prova/demo/exception/NoExtExc.java
+++ demo/src/main/java/prova/demo/exception/NoExtExc.java
@@ -1,5 +1,10 @@
 package prova.demo.exception;
 
+/**
+ * @author Alessandra D'Anna 
+ * Tale classe esplicita il messaggio di errore in cui si incappa se nel Dropbox non sono presenti file che presentano l'estensione specificata
+ */
+
 public class NoExtExc extends Exception{
 
 	public NoExtExc() {
