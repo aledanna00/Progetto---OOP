498ca86e6d3737b013613724add527ff7661479a
Alessandra D'Anna <93262359+aledanna00@users.noreply.github.com>
2021-12-16 16:53:48 +0100

    Update NoFileExc.java

--- demo/src/main/java/prova/demo/exception/NoFileExc.java
+++ demo/src/main/java/prova/demo/exception/NoFileExc.java
@@ -1,8 +1,12 @@
 package prova.demo.exception;
-
+/**
+ * @author Alesandra D'Anna
+ * Tale classe esplicita il messaggio di errore in cui si incappa se l'ArrayList di DropboxFile (che contiene gli elementi del DropBox) è vuoto
+ */
 public class NoFileExc extends Exception{
 	
 	public  NoFileExc(){
 		super("ERRORE: nessun file presente ");
 	}
 }
+
