2a4fd81067d848fed0ffb4109f4baca2697a5818
Alessandra D'Anna <93262359+aledanna00@users.noreply.github.com>
2021-12-21 18:37:51 +0100

    Update and rename StatisticsMaxMinAvgTest.java to StatisticsMaxMinAvgTests.java

--- demo/src/test/java/prova/demo/service/StatisticsMaxMinAvgTests.java
+++ demo/src/test/java/prova/demo/service/StatisticsMaxMinAvgTests.java
@@ -5,6 +5,7 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 import java.util.ArrayList;
 
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -13,12 +14,7 @@ import prova.demo.exception.NoFileExc;
 import prova.demo.model.*;
 import prova.demo.service.*;
 
-public class StatisticsMaxMinAvgTest {
-	
-	GetDataFromDropbox caricaDati = new GetDataFromDropbox();
-	 ArrayList<DropboxFile> listaFile = caricaDati.getListaFile();
-	 ArrayList<DropboxFolder> listaCartelle =caricaDati.getListaCartelle();
-	 ArrayList <DropboxFile> listaProva = new ArrayList<>();
+public class StatisticsMaxMinAvgTests { 
 	 StatisticsMaxMinAvg esempio = new StatisticsMaxMinAvg();
 	 @BeforeEach
 		void setUp() throws Exception {		
@@ -31,11 +27,20 @@ public class StatisticsMaxMinAvgTest {
 	 @Test
 	 @DisplayName("Test 1: Stampa del Massimo") 
 	 void test1() throws NoFileExc{
-			if(listaFile.isEmpty()) throw new NoFileExc();
-			else {
-				System.out.println(esempio.risultatoMassimo(".docx"));  
-				  assertNotNull(listaFile);
-			}
+		  int expected = 11059;
+		  Assertions.assertEquals(expected, esempio.risultatoMinimo(".docx"));
+		}
+	 @Test
+	 @DisplayName("Test 2: Stampa del Massimo") 
+	 void test2() throws NoFileExc{
+		  int expected = 11070;
+		  Assertions.assertEquals(expected, esempio.risultatoMassimo(".docx"));
+		}
+	 @Test
+	 @DisplayName("Test 3: Stampa del Massimo") 
+	 void test3() throws NoFileExc{
+		  int expected = 11062;
+		  Assertions.assertEquals(expected, esempio.risultatoMedia(".docx"));
 		}
 
 }
